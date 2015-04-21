209bf44dda541a775d2681d5c4035080c839c2e9
aalvarado <adan.alvarado7@gmail.com>
2015-04-21 16:47:17 -0700

    Example of setting new status

--- spec/app/cell_spec.rb
+++ spec/app/cell_spec.rb
@@ -5,5 +5,10 @@ describe Cell do
     it 'has a dead as default of 0 (dead)' do
       expect( cell.status ).to eql 0
     end
+
+    it 'can be have a 1 status (alive) ' do
+      cell.status = 1
+      expect( cell.status ).to eql 1
+    end
   end
 end
