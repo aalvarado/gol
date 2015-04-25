b775a7cee70967c3c26af39a3ecbf93513178eaf
aalvarado <adan.alvarado7@gmail.com>
2015-04-24 17:51:55 -0700

    Spawning cells condition example

--- spec/app/logic_spec.rb
+++ spec/app/logic_spec.rb
@@ -6,6 +6,8 @@ describe Logic do
   let( :logic ) { Logic.new(grid) }
 
   describe "Any live cell with fewer than two live neighbours" do
+    # 1 = alive
+    #
     # [1][0][0]
     # [0][1][0]
     # [0][0][0]
@@ -29,15 +31,17 @@ describe Logic do
   end
 
   describe "Any live cell with two or three live neighbours" do
-    # [ ][1][ ]
-    # [ ][1][ ]
-    # [ ][1][ ]
+    # 1 = alive
+    #
+    # [0][1][0]
+    # [0][1][0]
+    # [0][1][0]
     #
     # after step
     #
-    # [ ][ ][ ]
+    # [0][0][0]
     # [1][1][1]
-    # [ ][ ][ ]
+    # [0][0][0]
 
     before do
       cells[0][1].status = 1
@@ -59,10 +63,70 @@ describe Logic do
     end
   end
 
-  describe "Any live cell with more than three live neighbours dies, as if by overcrowding." do
+  describe "Any live cell with more than three live neighbours " do
+    # [1][1][1]
+    # [0][1][1]
+    # [0][1][0]
+    #
+    # after step
+    #
+    # [1][0][1]
+    # [0][0][0]
+    # [0][1][1]
+
+    let( :cell_statuses ) do
+      [
+        [0,1],
+        [1,1],
+        [1,2]
+      ]
+    end
+
+    before do
+      cells[0][0].status = 1
+      cells[0][1].status = 1
+      cells[0][2].status = 1
+      cells[1][1].status = 1
+      cells[1][2].status = 1
+      cells[2][1].status = 1
+      logic.step
+    end
+
+    it 'dies, as if by overcrowding.' do
+      cell_statuses.each do |i|
+        expect( cells[ i[0] ][ i[1] ].status ).to eq 0
+      end
+    end
   end
 
-  describe "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do
+  describe "Any dead cell with exactly three live neighbours " do
+    # [1][1][1]
+    # [0][0][0]
+    # [0][0][0]
+    #
+    # after step
+    #
+    # [1][1][1]
+    # [0][1][0]
+    # [0][0][0]
 
+    let( :cell_statuses ) do
+      [
+        [0,1],
+        [1,1],
+        [1,2]
+      ]
+    end
+
+    before do
+      cells[0][0].status = 1
+      cells[0][1].status = 1
+      cells[0][2].status = 1
+      logic.step
+    end
+
+    it 'becomes a live cell, as if by reproduction.' do
+      expect( cells[1][1].status ).to eq 1
+    end
   end
 end
