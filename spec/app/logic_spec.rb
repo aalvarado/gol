932f8f9380943c0a26b221a14a7c34146605240b
aalvarado <adan.alvarado7@gmail.com>
2015-04-24 17:23:50 -0700

    Add failing example on logic against cell life logic

--- spec/app/logic_spec.rb
+++ spec/app/logic_spec.rb
@@ -5,7 +5,17 @@ describe Logic do
   let( :cells ) { grid.cells }
   let( :logic ) { Logic.new(grid) }
 
-  describe "Any live cell with fewer than two live neighbours " do
+  describe "Any live cell with fewer than two live neighbours" do
+    # [1][0][0]
+    # [0][1][0]
+    # [0][0][0]
+    #
+    # after step
+    #
+    # [0][0][0]
+    # [0][0][0]
+    # [0][0][0]
+
     before do
       cells[0][0].status = 1
       cells[1][1].status = 1
@@ -13,11 +23,40 @@ describe Logic do
     end
 
     it 'dies' do
+      expect( cells[0][0].status ).to be 0
       expect( cells[1][1].status ).to be 0
     end
   end
 
-  describe "Any live cell with two or three live neighbours lives on to the next generation." do
+  describe "Any live cell with two or three live neighbours" do
+    # [ ][1][ ]
+    # [ ][1][ ]
+    # [ ][1][ ]
+    #
+    # after step
+    #
+    # [ ][ ][ ]
+    # [1][1][1]
+    # [ ][ ][ ]
+
+    before do
+      cells[0][1].status = 1
+      cells[1][1].status = 1
+      cells[2][1].status = 1
+
+      logic.step
+    end
+
+    it 'lives on to the next generation' do
+
+      [[0,1],[2,1]].each do |i|
+        expect( cells[ i[0] ][ i[1] ].status ).to eq 0
+      end
+
+      [[1,0],[1,1],[1,2]].each do |i|
+        expect( cells[ i[0] ][ i[1] ].status ).to eq 1
+      end
+    end
   end
 
   describe "Any live cell with more than three live neighbours dies, as if by overcrowding." do
